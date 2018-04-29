e0aacad38ee64032d61b95dab073dfb210bbd775
Ciaran Melvin <melvinc920515@aol.com>
2018-04-29 20:51:49 +0100

    a simple game of Blackjack

--- Blackjack.py
+++ Blackjack.py
@@ -56,6 +56,7 @@ def evaluator(hand):
             points += hand[a].value
     return points
 
+#Asks player if they want to play before entering game loop
 play = False
 print("Welcome to Blackjack!")
 playgame = raw_input("Would you like to play a game? [Y/N] ").upper()
@@ -63,13 +64,13 @@ if playgame == "Y":
     play = True
 
 while play == True:
-	#Inner loop to run through game, so when broken, the endgame code is always run on the outer loop
+    #Inner loop to run through game, so when broken, the endgame code is always run on the outer loop
     while True:
     
         newdeck = Deck()
         newdeck.shuffle()
-		
-		#Draws 2 cards for the player and the dealer
+        
+        #Draws 2 cards for the player and the dealer
         playerhand = []
         for a in range(0,2):
             newcard = newdeck.draw()
@@ -80,11 +81,11 @@ while play == True:
             newcard = newdeck.draw()
             dealerhand.append(newcard)
             
-		#Determines the value of each hand and assigns it to playerpts/dealerpts
-		playerpts = evaluator(playerhand)
+        #Determines the value of each hand and assigns it to playerpts/dealerpts
+        playerpts = evaluator(playerhand)
         dealerpts = evaluator(dealerhand)
         
-		#Checks if either player has Blackjack, and if so, ends the game
+        #Checks if either player has Blackjack, and if so, ends the game
         if playerpts == 21:
             print("Blackjack! You win!")
             break
@@ -93,18 +94,18 @@ while play == True:
             print("Dealer has Blackjack. You lose.")
             break
             
-		#Reveals one of the dealer's cards
+        #Reveals one of the dealer's cards
         print("The dealer has:\nX")
         dealerhand[0].reveal()  
             
-		#While the player has a valid hand and hasn't chosen to stick
+        #While the player has a valid hand and hasn't chosen to stick
         while playerpts < 22:
         
             print("You have:")
             for a in range(0,len(playerhand)):
                 playerhand[a].reveal()
-			
-			#Asks if they want to Stick or Twist. If Twist, new card is drawn and added to player's hand
+            
+            #Asks if they want to Stick or Twist. If Twist, new card is drawn and added to player's hand
             sticktwist = raw_input("Would you like to Stick or Twist? [S/T] ").upper()
             if sticktwist == "S":
                 break
@@ -114,21 +115,21 @@ while play == True:
                 print("You have drawn:")
                 newcard.reveal()
             
-			#Checks if player has bust and declares them a loser if so
+            #Checks if player has bust and declares them a loser if so
             playerpts = evaluator(playerhand)
             if playerpts > 21:
                 print("You're bust! You lose.")
          
-		#Checks again if player has bust to allow it to break the loop if so
+        #Checks again if player has bust to allow it to break the loop if so
         if playerpts > 21:
             break
         
-		#Now that the player's turns are over, both the dealer's cards are revealed
+        #Now that the player's turns are over, both the dealer's cards are revealed
         print("The dealer has:")
         for a in range(0,len(dealerhand)):
             dealerhand[a].reveal()
         
-		#If the dealer has less than 17 points, they must draw a card. Otherwise they Stick
+        #If the dealer has less than 17 points, they must draw a card. Otherwise they Stick
         while dealerpts < 17:
         
             newcard = newdeck.draw()
@@ -136,12 +137,12 @@ while play == True:
             newcard.reveal()
             dealerpts = evaluator(dealerhand)
          
-		#Checks if dealer has now bust, and breaks the loop if so
+        #Checks if dealer has now bust, and breaks the loop if so
         if dealerpts > 21:
             print("Dealer is bust! You win!")
             break
         
-		#If neither player or dealer has bust by now, a final comparison of scores to determine the winner, and ends the inner loop
+        #If neither player or dealer has bust by now, a final comparison of scores to determine the winner, and ends the inner loop
         if playerpts > dealerpts:
             print("You win!")
         elif playerpts < dealerpts:
@@ -150,7 +151,7 @@ while play == True:
             print("Tie game!")
         break
     
-	#Reveals player's and dealer's final hands
+    #Reveals player's and dealer's final hands
     print("You had: ")
     for a in range(0,len(playerhand)):
         playerhand[a].reveal()
@@ -158,7 +159,7 @@ while play == True:
     for a in range(0,len(dealerhand)):
         dealerhand[a].reveal()
     
-	#Asks player for another game. If not, then outer loop ends and the game ends
+    #Asks player for another game. If not, then outer loop ends and the game ends
     playagain = raw_input("Would you like to play again? [Y/N] ").upper()
     if playagain == "N":
         print("Goodbye!")
